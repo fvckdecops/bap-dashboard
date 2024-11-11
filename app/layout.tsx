b81532e33683a2910e3bf6c8b4b9d9bf3ff77c68
Xeno <admin@xeno-2.local>
2024-11-11 15:28:20 +0700

    Add Google Tag Manager

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import Header from "@/components/layouts/header";
 import { cn } from "@/components/Helpers";
 import { Toaster } from "sonner";
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import { GoogleTagManager } from '@next/third-parties/google'
 
 const DosisFonts = Dosis({
   subsets: ['latin'],
@@ -21,19 +22,23 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
   return (
     <html lang="en" className="m-0 p-0" suppressHydrationWarning>
       <body className={cn(
         DosisFonts.className,
         "w-screen h-screen pb-4"
       )}>
-        <noscript dangerouslySetInnerHTML={{__html: '<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WDQJ4L4" height="0" width="0" style="display: \'none\'; visibility: \'hidden\'"></iframe>'}} />
         <header className="w-screen mb-4">
           <Header />
         </header>
         <div>{children}</div>
         <Toaster />
         <SpeedInsights />
+        <GoogleTagManager 
+          gtmId={process.env.NEXT_PUBLIC_GA_ID!}
+          gtmScriptUrl={"https://www.googletagmanager.com/ns.html?id="+ process.env.NEXT_PUBLIC_GA_ID}
+        />
       </body>
     </html>
   );
